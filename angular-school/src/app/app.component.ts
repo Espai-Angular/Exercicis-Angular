76bcb7c0ab0c798eff682dd2bd4b9e820c0fe842
llopFilms <llopfilms@gmail.com>
2023-09-15 23:06:08 +0200

    Actualització

--- angular-school/src/app/app.component.ts
+++ angular-school/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-school';
+  meuNom: string | undefined = 'Pere';
+  mevaImatge: string | undefined = "../assets/logo-angular.png";
+  onDiguesHola($event: string) {
+    console.log($event);
+    
+  }
 }
--- angular-school/src/app/hello/hello.component.ts
+++ angular-school/src/app/hello/hello.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
   templateUrl: './hello.component.html',
-  styleUrls: ['./hello.component.css']
+  styleUrls: ['./hello.component.css'],
 })
 export class HelloComponent {
-
+  @Input() nom: string | undefined;
+  @Output() diguesHola: EventEmitter<string> = new EventEmitter<string>();
+  llistaNoms: string[] = [];
+  nomInput: string = "";
+  constructor() {}
+  onNomClick() {
+    this.diguesHola.emit('Molt bones, matí o tarda, per consola!');
+  }
+  afegirNom() {
+    this.llistaNoms.push(this.nomInput);
+    this.nomInput = "";
+    console.log(this.llistaNoms);    
+  }
 }
