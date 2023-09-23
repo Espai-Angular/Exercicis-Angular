3a5f76ca64d6dd2aa4561b291348fc6b30fbc1dc
llopFilms <llopfilms@gmail.com>
2023-09-23 12:53:33 +0200

    Actualització

--- Angular-28dies/src/app/app.component.ts
+++ Angular-28dies/src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnChanges, OnInit, OnDestroy {
   title = 'Angular, 28 dies';
   url: string = '../assets/th.jpeg';
   nom!: string;
-  ciutats: string[] = ['Lima', 'Monevideo', 'Cusco'];
+  ciutats: string[] = ['Lima', 'Montevideo', 'Cusco'];
+  ciutatSeleccionada!: string;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Canvis -->', changes);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngOnDestroy(): void {
+    console.log('Destroy');
+  }
+
+  onCiutatClicada(ciutat: string) :void {
+    console.log("Ciutat -->", ciutat);    
+    this.ciutatSeleccionada = ciutat;
+  }
+
+  onEsborrarSeleccio(): void {
+    this.ciutatSeleccionada = '';
+  }
 }
